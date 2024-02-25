e31e1f7bc32f461c429fa2249d998455b82f9753
Tran Hoang Nhu <tran.hoang.nhu.1997@gmail.com>
2024-02-25 21:22:01 +0700

    first commit

---
+++ routers/account.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const AccountModel = require("./models/account");
+
+// get all account
+router.get("/", (req, res, next) => {
+  AccountModel.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json("loi server");
+    });
+});
+
+// register account
+router.post("/", (req, res, next) => {});
+
+// update account
+router.put("/", (req, res, next) => {});
+
+// delete account
+router.delete("/", (req, res, next) => {});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const AccountModel = require("./models/account");
+// const accountRouter = require("./routers/account");
+const port = 8000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// endpoint:register account
+app.post("/register", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  AccountModel.findOne({
+    username: username,
+  })
+    .then((data) => {
+      if (data) {
+        res.json("user nay da ton tai");
+      } else {
+        return AccountModel.create({
+          username: username,
+          password: password,
+        });
+      }
+    })
+    .then((data) => {
+      res.json("tao tai khoan thanh cong !");
+    })
+    .catch((err) => {
+      res.status(500).json("tao tai khoan that bai !");
+    });
+});
+
+// endpoint:login account
+app.post("/login", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  AccountModel.findOne({
+    username: username,
+    password: password,
+  })
+    .then((data) => {
+      if (data) {
+        res.json("dang nhap thanh cong");
+      } else {
+        res.status(400).json("dang nhap that bai !");
+      }
+    })
+    .then((data) => {
+      res.json("tao tai khoan thanh cong !");
+    })
+    .catch((err) => {
+      res.status(500).json("co loi ben server !");
+    });
+});
+
+// app.use("/api/account/", accountRouter);
+
+// app.get("/", (req, res, next) => {
+//   res.json("HOME");
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
